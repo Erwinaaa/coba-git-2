b2d1979ffff63e52a2024a20a9c765fa129ad063
Erwinaaa <erwinapml@gmail.com>
2025-01-16 09:00:49 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis quia amet quam dolorum voluptate odit, eum eveniet tempora ut doloribus, exercitationem nobis saepe nihil accusamus blanditiis, laborum cupiditate animi dicta!</p>
 </body>
 </html>
